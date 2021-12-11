196d24640aff422896f59c0cc55176781c1c43e4
brisk286 <1459588761@qq.com>
2021-12-11 14:05:23 +0800

    explain

--- rsync.go
+++ rsync.go
@@ -3,74 +3,104 @@
 // Use of this source code is governed by a BSD-style license that can be found
 // in the LICENSE file.
 
-// A Golang implementation of the rsync algorithm.
+// Package rsync A Golang implementation of the rsync algorithm.
 // This package contains the algorithm for both client and server side.
 package rsync
 
-import "crypto/md5"
-
-const (
-	BlockSize = 1024 * 64
-	M         = 1 << 16
+import (
+	"crypto/md5"
 )
 
+const (
+	//默认块大小
+	//BlockSize = 1024 * 644
+	BlockSize = 2
+	//65536 弱哈希算法取模
+	M = 1 << 16
+)
+
+//hash块结构
 type BlockHash struct {
-	index      int
+	//哈希块下标
+	index int
+	//强哈希值
 	strongHash []byte
-	weakHash   uint32
+	//弱哈希值
+	weakHash uint32
 }
 
 // There are two kind of operations: BLOCK and DATA.
 // If a block match is found on the server, a BLOCK operation is sent over the channel along with the block index.
-// Modified data between two block matches is sent like a DATA operation. 
+// Modified data between two block matches is sent like a DATA operation.
+//常量
 const (
+	//数据传输
 	BLOCK = iota
+	//数据修改
 	DATA
 )
 
-// An rsync operation (typically to be sent across the network). It can be either a block of raw data or a block index.
+// RSyncOp An rsync operation (typically to be sent across the network). It can be either a block of raw data or a block index.
+//rsync数据体
 type RSyncOp struct {
 	// Kind of operation: BLOCK | DATA.
+	//操作类型：DATA是不完整的块  BLOCK是完整的块
 	opCode int
 	// The raw modificated (or misaligned) data. Iff opCode == DATA, nil otherwise.
+	//如果是DATA 那么保存数据
 	data []byte
 	// The index of found block. Iff opCode == BLOCK. nil otherwise.
+	//如果是BLOCK 保存块下标
 	blockIndex int
 }
 
-// Returns weak and strong hashes for a given slice.
+// CalculateBlockHashes Returns weak and strong hashes for a given slice.
+//计算每个块的哈希值
+//参数：全部数据内容
+//返回：每个块组成的列表
 func CalculateBlockHashes(content []byte) []BlockHash {
 	blockHashes := make([]BlockHash, getBlocksNumber(content))
 	for i := range blockHashes {
 		initialByte := i * BlockSize
 		endingByte := min((i+1)*BlockSize, len(content))
+		// 确认每个块的定位
 		block := content[initialByte:endingByte]
 		weak, _, _ := weakHash(block)
+		//保存到块哈希数组中
 		blockHashes[i] = BlockHash{index: i, strongHash: strongHash(block), weakHash: weak}
 	}
 	return blockHashes
 }
 
 // Returns the number of blocks for a given slice of content.
+//计算文件需要块的数量
 func getBlocksNumber(content []byte) int {
-	blockNumber := (len(content) / BlockSize)
+	blockNumber := len(content) / BlockSize
 	if len(content)%BlockSize != 0 {
 		blockNumber += 1
 	}
 	return blockNumber
 }
 
-// Applies operations from the channel to the original content.
+// ApplyOps Applies operations from the channel to the original content.
 // Returns the modified content.
+//根据通道接收到的信息，将数据组装发送
+//参数：文件内容，数据操作体 通道， 本地文件大小
+//返回:组装后的数据
 func ApplyOps(content []byte, ops chan RSyncOp, fileSize int) []byte {
 	var offset int
 	result := make([]byte, fileSize)
+	//遍历通道接收到的数据
 	for op := range ops {
 		switch op.opCode {
 		case BLOCK:
+			//copy：目的地，源文件
+			//fmt.Println(1, content[op.blockIndex*BlockSize:op.blockIndex*BlockSize+BlockSize])
 			copy(result[offset:offset+BlockSize], content[op.blockIndex*BlockSize:op.blockIndex*BlockSize+BlockSize])
 			offset += BlockSize
+		//DATA是不定长的
 		case DATA:
+			//fmt.Println(2, op.data)
 			copy(result[offset:], op.data)
 			offset += len(op.data)
 		}
@@ -78,57 +108,82 @@ func ApplyOps(content []byte, ops chan RSyncOp, fileSize int) []byte {
 	return result
 }
 
-// Computes all the operations needed to recreate content.
+// CalculateDifferences Computes all the operations needed to recreate content.
 // All these operations are sent through a channel of RSyncOp.
+//不返回，将处理的数据放入通道
+//参数：本地文件内容， 传送过来的块哈希数组， 空操作通道
 func CalculateDifferences(content []byte, hashes []BlockHash, opsChannel chan RSyncOp) {
 
+	//构建一个哈希map，<下标，哈希块列表>？ 链表结构？
 	hashesMap := make(map[uint32][]BlockHash)
 	defer close(opsChannel)
 
+	//遍历每个哈希块数组
 	for _, h := range hashes {
 		key := h.weakHash
+		//用弱hash做key，值为哈希块
 		hashesMap[key] = append(hashesMap[key], h)
 	}
 
+	//移动下标  前一个匹配块的尾部
 	var offset, previousMatch int
+	//弱hash 3个数值
 	var aweak, bweak, weak uint32
+	//标记
 	var dirty, isRolling bool
 
 	for offset < len(content) {
+		//一个块的尾部
 		endingByte := min(offset+BlockSize, len(content)-1)
 		block := content[offset:endingByte]
+		//如果不用rolling
 		if !isRolling {
+			//弱hash的三个值
 			weak, aweak, bweak = weakHash(block)
+			//如果没找到对应的块  下一次进行rolling
 			isRolling = true
+			//如果一直找不到会一直rolling，直到找个能对应的块，两个能对应的块之间都是DATA
 		} else {
+			//rolling操作 计算下一个step 1 的hash值
 			aweak = (aweak - uint32(content[offset-1]) + uint32(content[endingByte-1])) % M
 			bweak = (bweak - (uint32(endingByte-offset) * uint32(content[offset-1])) + aweak) % M
 			weak = aweak + (1 << 16 * bweak)
 		}
+		//如果在hashmap中找到了弱hash对应的块， 弱hash找用hashmap
 		if l := hashesMap[weak]; l != nil {
+			//强hash找用遍历
 			blockFound, blockHash := searchStrongHash(l, strongHash(block))
+			//如果从hash块队列中找到了强hash块
 			if blockFound {
+				//如果是DATA
 				if dirty {
+					//将一个数组操作体放入操作管道中
 					opsChannel <- RSyncOp{opCode: DATA, data: content[previousMatch:offset]}
 					dirty = false
 				}
+				//将一个数组操作体放入操作管道中
 				opsChannel <- RSyncOp{opCode: BLOCK, blockIndex: blockHash.index}
 				previousMatch = endingByte
+				// 找到了就不用rolling
 				isRolling = false
 				offset += BlockSize
 				continue
 			}
 		}
+		//如果找不到弱hash对应的块 将下一轮搜索的块标记为DATA
 		dirty = true
+		//rolling
 		offset++
 	}
 
+	//如果最后一个块不对应,那么把所有DATA放入
 	if dirty {
 		opsChannel <- RSyncOp{opCode: DATA, data: content[previousMatch:]}
 	}
 }
 
 // Searches for a given strong hash among all strong hashes in this bucket.
+//从hash块队列中遍历每个块的强hash值  一一比对
 func searchStrongHash(l []BlockHash, hashValue []byte) (bool, *BlockHash) {
 	for _, blockHash := range l {
 		if string(blockHash.strongHash) == string(hashValue) {
@@ -146,6 +201,7 @@ func strongHash(v []byte) []byte {
 }
 
 // Returns a weak hash for a given block of data.
+//弱hash
 func weakHash(v []byte) (uint32, uint32, uint32) {
 	var a, b uint32
 	for i := range v {
--- rsync_test.go
+++ rsync_test.go
@@ -6,7 +6,10 @@
 // Unit tests for core package
 package rsync
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 import "io/ioutil"
 
 type filePair struct {
@@ -16,18 +19,24 @@ type filePair struct {
 
 func Test_SyncModifiedContent(t *testing.T) {
 
+	//文件队列表
 	files := []filePair{filePair{"golang-original.bmp", "golang-modified.bmp"}, filePair{"text-original.txt", "text-modified.txt"}}
 
 	for _, filePair := range files {
+		//根据文件名读文件内容
 		original, _ := ioutil.ReadFile("test-data/" + filePair.original)
 		modified, _ := ioutil.ReadFile("test-data/" + filePair.modified)
 
+		//计算每个块的哈希值
 		hashes := CalculateBlockHashes(original)
 		opsChannel := make(chan RSyncOp)
 		go CalculateDifferences(modified, hashes, opsChannel)
 
 		result := ApplyOps(original, opsChannel, len(modified))
 
+		fmt.Println(result)
+		fmt.Println(modified)
+		//modified为正确答案
 		if string(result) != string(modified) {
 			t.Errorf("rsync did not work as expected for %v", filePair)
 		}
@@ -44,6 +53,43 @@ func Test_WeakHash(t *testing.T) {
 	assertHash(t, "weak", content, expectedWeak, weak)
 	assertHash(t, "a", content, expectedA, a)
 	assertHash(t, "b", content, expectedB, b)
+}
+
+func Test_Result(t *testing.T) {
+	//文件队列表
+	files := []filePair{filePair{"text-original.txt", "text-modified.txt"}}
+
+	for _, filePair := range files {
+		//根据文件名读文件内容
+		original, _ := ioutil.ReadFile("test-data/" + filePair.original)
+		modified, _ := ioutil.ReadFile("test-data/" + filePair.modified)
+
+		fmt.Println(original, modified)
+		//[115 111 109 101 32 116 101 120 116 13 10]
+		//[115 111 109 101 32 101 120 116 114 97 32 116 101 120 116 13 10]
+
+		//计算每个块的哈希值
+		//发送方计算哈希块列表，将它发送给接收方
+		hashes := CalculateBlockHashes(original)
+
+		opsChannel := make(chan RSyncOp)
+
+		//接收方接收发送方的hash队列，将其用哈希表储存，本地的文件按照块大小与其对比
+		//将结果装在通道中，通道中装的是 本地文件按发送方hash队列哈希表的映射
+		//DATA:data  BLOCK: index
+		go CalculateDifferences(modified, hashes, opsChannel)
+
+		//接收方将通道返回，发送方根据接收方的通道将文件重新组装
+		//其中data是文件的修改部分，block以index的形式复原
+		result := ApplyOps(original, opsChannel, len(modified))
+
+		fmt.Println(result, modified)
+		//fmt.Println()
+		//modified为正确答案
+		if string(result) != string(modified) {
+			t.Errorf("rsync did not work as expected for %v", filePair)
+		}
+	}
 }
 
 func assertHash(t *testing.T, name string, content []byte, expected uint32, found uint32) {
